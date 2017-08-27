09cbaf8919390a47faf77db6c2dcfb8c56c86f84
ufda <zzs888@hotmail.com>
2017-08-27 23:42:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.get('/hello', function(req, res) {
-    return res.json({
-        speech: 'speeck',
-        displayText: 'Hi ZZS',
-        source: pg_connect()
-    });
+    return 'Hello World';
 });
 
 restService.post('/echo', function(req, res) {
@@ -143,9 +139,7 @@ function bimbqm(){
 }
 
 function pg_connect(){
-    #var pg = require('pg');
-    #pg.defaults.ssl = true;
-    var conString = "postgres://nlmiijucugmkgy:ecee66c60fdf4553ad10261ff5fa2d0bb65858f2a623be3559d73ad938f534d1@ec2-184-73-247-240.compute-1.amazonaws.com:5432/d8hm6a03dul7uh";
+    var conString = 'postgres://nlmiijucugmkgy:ecee66c60fdf4553ad10261ff5fa2d0bb65858f2a623be3559d73ad938f534d1@ec2-184-73-247-240.compute-1.amazonaws.com:5432/d8hm6a03dul7uh';
     return conString;
 }
 
