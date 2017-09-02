6141cd64e1423d43f2d9cda91e04d9854a4fc68f
ufda <zzs888@hotmail.com>
2017-09-03 00:37:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/slack-eiw', function(req, res) {
     var smsg = 'other message';
     
     if ( action && action == 'q_people'){
-        smsg = req.body.result.parameters.given-name;
+        smsg = req.body.result.parameters.TR;
     }
     return res.json({
         speech: smsg,
