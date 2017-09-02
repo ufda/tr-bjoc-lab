c80d60858c83cb17d87194eee578ef6397f250df
ufda <zzs888@hotmail.com>
2017-09-02 15:06:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -43,9 +43,8 @@ restService.get('/hello', function(req, res) {
     
 });
 
-restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "ZZS: Seems like some problem. Speak again."
-    var action = req.body.result.action;
+restService.get('/echo', function(req, res) {
+    var speech =  "ZZS: Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
