ca4eb56b623a40f4baec83a83060e98eb0065a6d
ZansongZhu <zansongzhu@tr-zzs-macbook-pro.local>
2017-10-01 18:30:08 +0800

    files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
+
 const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static(path.join(__dirname, 'files')))
