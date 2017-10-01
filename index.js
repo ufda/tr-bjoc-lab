eaf74f13607ee5c260587301d5174827bbdc38e8
ZansongZhu <zansongzhu@tr-zzs-macbook-pro.local.ten.thomsonreuters.com>
2017-10-01 18:08:52 +0800

    files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+app.use(express.static(path.join(__dirname, 'files')))
 
 app.use(bodyParser.urlencoded({
     extended: true
